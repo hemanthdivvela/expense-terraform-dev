90e8418b6a285a759c01e9841ddec144afb1549d
hemanthdivvela <divvelahemanth7@gmail.com>
2024-12-10 00:33:26 +0530

    function

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -2,7 +2,7 @@
 # user data will get sudo access
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/hemanthdivvela/expense-ansible-role.git
-cd expense-ansible-role
+git clone git@github.com:hemanthdivvela/expense-ansible-roles.git
+cd expense-ansible-roles
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
